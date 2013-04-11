215f4567f064ae978f8c6ed316367e7702b4db4c
Tristan Louet <tristan.louet.dev@gmail.com>
2013-04-12 01:04:52 +0200

    Mise à jour de deconnection.php
    Voir le précédent commit (Améliorations de la connexion)

--- deconnection.php
+++ deconnection.php
@@ -5,8 +5,9 @@ $connectionCookieName = 'connection';
 //On met le cookie dans le passé, afin de déconnecter le client 
 setcookie($connectionCookieName, "", time() - 3600);
 if (isset($_GET['src'])) {
-    header("Location: index.php");
+    header("Location: index.php?status=".$_GET['src']);
+} else {
+    header("Location: index.php?status=deco");
 }
-header("Location: index.php?status=deco");
 
 ?>
